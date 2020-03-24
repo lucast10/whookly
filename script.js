a8b6b1251021c746c8c082a6f48030ffe778658a
Luciano Castellucci <luciano.castellucci@globant.com>
2020-03-24 00:41:48 -0300

    Fixed JS logic and added responsiveness

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
+var dayOfMonth = new Date();
+var date = dayOfMonth.getDate();
 
+document.addEventListener("DOMContentLoaded", function () { 
+    if (date <= 7) {
+        console.log("hi1");
+        document.getElementById('segundoesquema').style.display = 'none';
+        document.getElementById('terceresquema').style.display = 'none';
+        document.getElementById('cuartoesquema').style.display = 'none';
+    } else if (date >= 8 && date <= 14) {
+        console.log("hi2");
+        document.getElementById('esquema').style.display = 'none';
+        document.getElementById('terceresquema').style.display = 'none';
+        document.getElementById('cuartoesquema').style.display = 'none';  
+    } else if (date >= 15 && date <= 21) {
+        console.log("hi3");
+        document.getElementById('esquema').style.display = 'none';    
+        document.getElementById('segundoesquema').style.display = 'none';
+        document.getElementById('cuartoesquema').style.display = 'none';
+    } else {
+        console.log("hi4");
+        document.getElementById('esquema').style.display = 'none';   
+        document.getElementById('segundoesquema').style.display = 'none';
+        document.getElementById('terceresquema').style.display = 'none';
+    }
+ });
 
